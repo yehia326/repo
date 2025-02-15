372a515c1e884a90540a55c53a9ddfe28e23ea8b
yehia326 <yehia.elgeretly1@gmail.com>
2025-02-15 17:00:09 +0200

    added welcoming message in main py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ print("select a number from 1-10")
 print("1-3:easy")
 print("4-7:medium")
 print("8-10:hard")
+
+
+choice = int(input("Enter your choice: "))
+print("the game is simple, you have to guess the number between 1-10")
+print("you have 3 chances to guess the number")
